d9c761883c89eb84b11ed1bed4278d61192cc481
Gabe Conradi <gummybearx@gmail.com>
2013-01-14 23:27:59 -0500

    setting up about page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-
-/*
- * GET home page.
- */
-
+// home page
 exports.index = function(req, res){
-  res.render('index', { title: 'Byxor.na' });
+  res.render('index', { title: 'byxor.na' });
+};
+
+// about me
+exports.about = function(req, res){
+  res.render('about', {title: 'About'});
+};
+
+// resume listing
+exports.resume = function(req, res){
+  res.render('resume', {title: 'Resume'});
 };
