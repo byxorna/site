c4e2676e329c3f16fe8305ef59944e0aa62859c9
Gabe Conradi <gummybearx@gmail.com>
2013-01-26 19:53:50 -0500

    set up blogging engine

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-//  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+  page = require('./routes/index'),
+  blog = require('./routes/blog'),
+  http = require('http'),
+  path = require('path'),
+  app = express(),
+  poet = require('poet')(app);
 
-var app = express();
+poet.set({
+  posts: './posts'
+})
+  .init(function(){
+  });
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(poet.middleware());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/about/me', routes.about_me);
-app.get('/projects', routes.projects);
-app.get('/resume', routes.resume);
+app.get('/', page.index);
+app.get('/about/me', page.about_me);
+app.get('/projects', page.projects);
+app.get('/resume', page.resume);
+app.get('/blog', blog.blog_index);
+app.get('/blog/post/:post', blog.post);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
