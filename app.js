7787b57b01dbc4c56340db898f582ec7fa6a0582
Gabe Conradi <gabe.conradi@gmail.com>
2021-08-07 12:19:33 -0400

    ugh, everything is broken, fucking javascript

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var blog;
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(require('method-override')());
